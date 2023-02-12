8936171bdf34259fbb7f2653fd04f82ad91ecb69
Alicoder001 <coderkuchkarov@gmail.com>
2023-02-12 13:34:32 +0500

    added

---
+++ precache-manifest.104c9a62f9f8990abb9277969aff8306.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e825c0e1d2dde44da02bfb466177c1a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f8ea33d6f467f28d3454",
+    "url": "/static/css/main.33f52212.chunk.css"
+  },
+  {
+    "revision": "126940c247cf3795b0a3",
+    "url": "/static/js/2.5bf38f03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5bf38f03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8ea33d6f467f28d3454",
+    "url": "/static/js/main.6cf15c78.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
